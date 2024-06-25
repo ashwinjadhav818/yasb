d478ce5ac59bccd590f12bde640a1d143f22e3c4
Kuukunen <kuukunen@users.noreply.github.com>
2024-06-26 01:49:34 +0900

    Fix problem where active_window widget was not detecting monitor.

--- src/core/widgets/base.py
+++ src/core/widgets/base.py
@@ -4,6 +4,7 @@ from PyQt6.QtWidgets import QWidget, QHBoxLayout, QFrame
 from PyQt6.QtGui import QMouseEvent
 from PyQt6.QtCore import QTimer, QThread, Qt
 from typing import Union
+from core.utils.win32.utilities import get_monitor_hwnd
 
 
 class BaseWidget(QWidget):
@@ -16,6 +17,9 @@ class BaseWidget(QWidget):
             class_name: str = ""
     ):
         super().__init__()
+
+        self.monitor_hwnd = get_monitor_hwnd(int(self.winId()))
+
         self._widget_frame = QFrame()
         self._widget_frame_layout = QHBoxLayout()
         self.widget_layout = QHBoxLayout()
