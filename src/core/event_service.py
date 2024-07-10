7087422ee67cf780e273a07f56ac0bc1a0fb3e2f
Kuukunen <kuukunen@users.noreply.github.com>
2024-07-10 22:55:48 +0900

    Replace QRecursiveMutex with threading.RLock because of reported issues.

--- src/core/event_service.py
+++ src/core/event_service.py
@@ -1,9 +1,10 @@
 import functools
 import logging
 
-from PyQt6.QtCore import QObject, pyqtSignal, QRecursiveMutex, QMutexLocker
+from PyQt6.QtCore import QObject, pyqtSignal
 from typing import Any
 from core.event_enums import Event
+from threading import RLock
 
 
 @functools.lru_cache()
@@ -11,25 +12,25 @@ class EventService(QObject):
     def __init__(self):
         super().__init__()
         self._registered_event_signals: dict[Event, list[pyqtSignal]] = {}
-        self._mutex = QRecursiveMutex()
+        self._mutex = RLock()
 
     def register_event(self, event_type: Event, event_signal: pyqtSignal):
-        _lock = QMutexLocker(self._mutex)
-        if event_type not in self._registered_event_signals:
-            self._registered_event_signals[event_type] = [event_signal]
-        else:
-            self._registered_event_signals[event_type].append(event_signal)
+        with self._mutex:
+            if event_type not in self._registered_event_signals:
+                self._registered_event_signals[event_type] = [event_signal]
+            else:
+                self._registered_event_signals[event_type].append(event_signal)
 
     def emit_event(self, event_type: Event, *args: Any):
-        _lock = QMutexLocker(self._mutex)
-        event_signals = self._registered_event_signals.get(event_type, [])
-        for event_signal in event_signals:
-            try:
-                event_signal.emit(*args)
-            except AttributeError:
-                logging.error(f"Failed to emit signal {event_signal.__str__()}. Removing link to {event_type}.")
-                event_signals.pop(event_signals.index(event_signal))
+        with self._mutex:
+            event_signals = self._registered_event_signals.get(event_type, [])
+            for event_signal in event_signals:
+                try:
+                    event_signal.emit(*args)
+                except AttributeError:
+                    logging.error(f"Failed to emit signal {event_signal.__str__()}. Removing link to {event_type}.")
+                    event_signals.pop(event_signals.index(event_signal))
 
     def clear(self):
-        _lock = QMutexLocker(self._mutex)
-        self._registered_event_signals.clear()
+        with self._mutex:
+            self._registered_event_signals.clear()
